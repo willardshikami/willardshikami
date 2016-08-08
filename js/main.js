018d1cb16c3861826b6113e5c0739e43745640bc
willardshikami <willardshikami@gmail.com>
2016-08-08 22:08:16 +0300

    added smooth scrolling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-var nb = $ (".nav-bar");
+$(document).ready(function () {
+    // bind click event to all internal page anchors
+    $('a[href*="#"]').on('click', function (e) {
+        // prevent default action and bubbling
+        e.preventDefault();
+        e.stopPropagation();
+        // set target to anchor's "href" attribute
+        var target = $(this).attr('href');
+        // scroll to each target
+        $(target).velocity('scroll', {
+            duration: 500,
+            offset: -40,
+            easing: 'ease-in-out'
+        });
+    });
+});
